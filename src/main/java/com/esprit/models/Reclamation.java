b2760e88fa4d413e111270051efc0aec741f0532
Aymen-Ftita <aymenftita1@gmail.com>
2024-02-10 11:42:48 +0100

    g

--- src/main/java/com/esprit/models/Reclamation.java
+++ src/main/java/com/esprit/models/Reclamation.java
@@ -2,7 +2,14 @@ package com.esprit.models;
 
 public class Reclamation {
 
-    
+    private int id_reclamation;
+    private int utilisateur_id;
+    private String date_creation;
+    private String sujet;
+    private String description;
+    private Role status;
+    private int priorité;
+    private Role responsablle;
 
     public Reclamation() {
     }
